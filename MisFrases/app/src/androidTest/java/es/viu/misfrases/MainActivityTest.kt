b4ce2e1717da6ced9b3a81f4c19435b5aa2a1272
Paco Mastermind <paco@mastermind.ac>
2024-03-24 09:18:29 +0100

    UI Testing Android

---
+++ MisFrases/app/src/androidTest/java/es/viu/misfrases/MainActivityTest.kt
@@ -0,0 +1,28 @@
+package es.viu.misfrases
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import es.viu.misfrases.view.MainActivity
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun cambiarFraseUI(){
+        onView(withId(R.id.viewContainer)).perform(click())
+        onView(withId(R.id.tvFrase)).check(matches(not(withText(""))))
+    }
+}
